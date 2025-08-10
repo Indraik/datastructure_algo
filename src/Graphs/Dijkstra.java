17ee163715129f981185205f95b9dbffafb7214b
indhar <indhar306@gmail.com>
2025-08-10 16:22:45 +0530

    graph

--- src/Graphs/Dijkstra.java
+++ src/Graphs/Dijkstra.java
@@ -1,9 +1,25 @@
 package Graphs;
 
+class DijkstraGraphMatrix{
+	static int[][] adjMAtrix;
+	static int numVert;
+	public DijkstraGraphMatrix(int numVert) {
+		DijkstraGraphMatrix.numVert = numVert;
+		adjMAtrix = new int[numVert][numVert];
+	}
+	public static void addEdge(int src,int dest,int weight) {
+		adjMAtrix[src][dest] = weight;
+	}
+	public static void dijkstra() {
+		
+	}
+}
+
 public class Dijkstra {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
 
 	}
 
